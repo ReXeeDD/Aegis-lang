de59b12c18e35ec54ede13172bd50c8b5a250437
rii125-125 <rii125.idd@gmail.com>
2026-03-20 17:18:22 +0900

    Implementation of a "loop that processes one character at a time"

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include "ast/lexer.hpp"
+#include "token.hpp"
 
 int main() {
-    std::cout << "Hello Aegis-lang!" << std::endl;
+    // Test code
+    std::string_view source =
+        "fun main() {\n"
+        "    wtr x = 10;\n"
+        "    print \"Hello, Aegis!\";\n"
+        "}";
+
+    std::cout << "--- Scanning Code ---" << std::endl;
+    std::cout << source << std::endl;
+    std::cout << "---------------------" << std::endl;
+
+    aegis::Lexer lexer(source);
+    std::vector <aegis::Token> tokens = lexer.tokenize();
+
+    for (const auto& token : tokens) {
+        std::cout << "Line" << token.line << "|";
+        // Display a number using TokenType
+        std::cout << "Type: " << static_cast<int>(token.type) << "|";
+        std::cout << "Literal: [" << token.literal << "]" << std::endl;
+    }
+
     return 0;
 }
